dad5e113deb4f990427de6fcbf74091c241878a7
lrxu <18352868167@163.com>
2020-03-25 17:28:36 +0800

    Q1补充

--- imgUpload/Q1/index.js
+++ imgUpload/Q1/index.js
@@ -1,7 +1,9 @@
-$(document).ready(function(){ 
-  console.log('onready');
-});
-
-function selectFile() {
-  console.log()
+function selectFile(event) {
+  let tempFile = document.getElementById('fileUpload').files[0]
+  var reads = new FileReader();
+  reads.readAsDataURL(tempFile);
+  reads.onload = function(e) {
+    document.querySelector("img").style.display = "block"
+    document.querySelector("img").src=e.target.result;
+  }
 }
